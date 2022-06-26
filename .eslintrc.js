647862e169bbda14bec396e2e4636d1c89903988
su100 <zxctys@naver.com>
2022-06-26 17:16:37 +0900

    feat: eslint 설정 변경(button props)

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,7 @@ module.exports = {
     'react/prop-types': 0,
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'react/prop-types': 0,
+    'react/button-has-type': 'off',
   },
 };
